7c9e1bb953f1116074dc696b4b9e88250b434fd6
YukihiroArakawa <yukiburos@icloud.com>
2021-03-08 16:15:38 +0900

    refactoring circle search header

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get    'password_resets/new'
   get    'password_resets/edit'
   get    'sessions/new'
-  root   'static_pages#home'
+  root   'search_circles#index'
   get    '/help',     to: 'static_pages#help'
   get    '/about',    to: 'static_pages#about'
   get    '/contact',  to: 'static_pages#contact'
